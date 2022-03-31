133ab36588142b452b9bfa8082924a36564d9954
tclarsson <thomas@tclarsson.se>
2022-03-31 13:58:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
     Thomas was here
-    </br>
+    
     
           Get started by editing <code>pages/index.js</code>
         </p>
